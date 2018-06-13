5e479f89915a5021d0dd0feb103c2dd4e3122c14
SyedFarhan <syedrfarhan2@gmail.com>
2018-06-13 11:11:24 -0400

    Cleaned up code, added shirt and pants proptypes

--- src/containers/OutfitMaker.js
+++ src/containers/OutfitMaker.js
@@ -3,30 +3,15 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import { addGarment, deleteGarment, getGarments, getMeals, setError, getShirts, getPants } from '../actions/garments';
+import { getShirts, getPants } from '../actions/garments';
 
 class OutfitMaker extends Component {
   static propTypes = {
     Layout: PropTypes.func.isRequired,
-    garments: PropTypes.shape({
-      loading: PropTypes.bool.isRequired,
-      error: PropTypes.string,
-      garments: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-    }).isRequired,
-    match: PropTypes.shape({
-      params: PropTypes.shape({}),
-    }),
-    deleteGarment: PropTypes.func.isRequired,
-    getGarments: PropTypes.func.isRequired,
-    getMeals: PropTypes.func.isRequired,
-    setError: PropTypes.func.isRequired,
-    addGarment: PropTypes.func.isRequired,
     getShirts: PropTypes.func.isRequired,
     getPants: PropTypes.func.isRequired,
-  }
-
-  static defaultProps = {
-    match: null,
+    shirts: PropTypes.shape().isRequired,
+    pants: PropTypes.shape().isRequired,
   }
 
   componentDidMount = () => {
@@ -35,19 +20,6 @@ class OutfitMaker extends Component {
     console.log(this.props.shirts);
     console.log(this.props.pants);
   };
-
-  /**
-   * Fetch Data from API, saving to Redux
-   */
-  fetchGarments = () => {
-    console.log('fetching garments');
-    return this.props.getGarments()
-      .then(() => this.props.getMeals())
-      .catch((err) => {
-        console.log(`Error: ${err}`);
-        return this.props.setError(err);
-      });
-  }
 
   fetchShirts = () => {
     console.log('fetching shirts');
@@ -66,18 +38,12 @@ class OutfitMaker extends Component {
   }
 
   render = () => {
-    const { Layout, garments, match, shirts, pants } = this.props;
-    const id = (match && match.params && match.params.id) ? match.params.id : null;
+    const { Layout, shirts, pants } = this.props;
 
     return (
       <Layout
-        error={garments.error}
-        loading={garments.loading}
-        garments={garments.garments}
         shirts={this.createArrayFromObjectProperties(shirts.shirts.byId)}
         pants={this.createArrayFromObjectProperties(pants.pants.byId)}
-        deleteGarment={this.props.deleteGarment}
-        addGarment={this.props.addGarment}
       />
     );
   }
@@ -91,11 +57,6 @@ const mapStateToProps = state => ({
 
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  addGarment,
-  getGarments,
-  deleteGarment,
-  getMeals,
-  setError,
   getShirts,
   getPants,
 }, dispatch);
--- src/native/components/OutfitMaker.js
+++ src/native/components/OutfitMaker.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import PropTypes from 'prop-types';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { FlatList, TouchableOpacity, RefreshControl, Image, ScrollView, Dimensions } from 'react-native';
-import { Container, Content, Card, CardItem, Body, Text, Button, DeckSwiper, Left, Thumbnail, Icon } from 'native-base';
-import { Actions } from 'react-native-router-flux';
-import Loading from './Loading';
-import Error from './Error';
-import Header from './Header';
+import { Card, CardItem, Body, Text, Button, DeckSwiper, Left, Thumbnail, Icon } from 'native-base';
 import Spacer from './Spacer';
 
 
 
 const OutfitMaker = ({
-  error,
-  loading,
-  garments,
   shirts,
-  reFetch,
-  deleteGarment,
   pants,
 }) => {
   // Loading
-  if (loading) return <Loading />;
+  // if (loading) return <Loading />;
 
   // Error
-  if (error) return <Error content={error} />;
+  // if (error) return <Error content={error} />;
 
   // const keyExtractor = item => item.id;
 
@@ -93,68 +83,8 @@ const OutfitMaker = ({
 };
 
 OutfitMaker.propTypes = {
-  error: PropTypes.string,
-  loading: PropTypes.bool.isRequired,
-  garments: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-  reFetch: PropTypes.func,
-};
-
-OutfitMaker.defaultProps = {
-  error: null,
-  reFetch: null,
+  shirts: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  pants: PropTypes.arrayOf(PropTypes.shape()).isRequired,
 };
 
 export default OutfitMaker;
-
-/*
-<Container style={{ flex: 1, height: (2 * (Dimensions.get('window').height)) }}>
-  <Content padder contentContainerStyle={{ flex: 1, justifyContent: 'space-between' }} >
-    <DeckSwiper
-      key={1}
-      dataSource={garments}
-      renderItem={item => (
-        <Card style={{ elevation: 3, flex: 1 }}>
-          <CardItem>
-            <Left>
-              <Body>
-              <Text>{item.title}</Text>
-              <Text note>{item.brand}</Text>
-              </Body>
-            </Left>
-          </CardItem>
-          <CardItem cardBody>
-            <Image style={{ height: 300, flex: 1 }} source={{ uri: item.imageUrl }} />
-          </CardItem>
-          <CardItem>
-            <Icon name="heart" style={{ color: '#ED4A6A' }} />
-            <Text>{item.name}</Text>
-          </CardItem>
-        </Card>)
-      }
-    />
-    <Spacer size={100} />
-    <DeckSwiper
-      key={2}
-      dataSource={garments}
-      renderItem={item => (
-        <Card style={{ elevation: 2 }}>
-          <CardItem>
-            <Left>
-              <Body>
-              <Text>{item.title}</Text>
-              <Text note>{item.brand}</Text>
-              </Body>
-            </Left>
-          </CardItem>
-          <CardItem cardBody>
-            <Image style={{ height: 300 }} source={{ uri: item.imageUrl }} />
-          </CardItem>
-          <CardItem>
-            <Icon name="heart" style={{ color: '#ED4A6A' }} />
-            <Text>{item.name}</Text>
-          </CardItem>
-        </Card>)
-      }
-    />
-  </Content>
-</Container> */
