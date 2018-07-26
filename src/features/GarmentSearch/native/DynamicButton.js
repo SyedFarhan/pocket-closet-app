841e77ca31a0ee988ac7860b9c94cdb40b0144cf
SyedFarhan <syedrfarhan2@gmail.com>
2018-07-25 21:59:30 -0400

    Unit tested dynamicbutton with snapshots

--- src/features/GarmentSearch/native/DynamicButton.js
+++ src/features/GarmentSearch/native/DynamicButton.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
 import PropTypes from 'prop-types';
 
 import SearchButton from './SearchButton';
-import ClearButton from './ClearButton'
+import ClearButton from './ClearButton';
 
-const DynamicButton = ({ onSearch, onClear, isSearched }) => {
+const DynamicButton = ({ isSearched, onSearch, onClear }) => {
   return (
     isSearched ? <ClearButton onClear={onClear} /> : <SearchButton onSearch={onSearch} />
   );
 };
 
-// DynamicButton.propTypes = {
-//   isSearched: PropTypes.bool.isRequired,
-// };
+DynamicButton.propTypes = {
+  isSearched: PropTypes.bool.isRequired,
+  onSearch: PropTypes.func,
+  onClear: PropTypes.func,
+};
 
 export default DynamicButton;
--- src/features/GarmentSearch/native/DynamicButton.test.js
+++ src/features/GarmentSearch/native/DynamicButton.test.js
@@ -1,10 +1,66 @@
-/* global describe, it, expect */
+/* global test */
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 import DynamicButton from './DynamicButton';
+import ClearButton from './ClearButton';
+import SearchButton from './SearchButton';
 
-test('renders SearchButton when isSearched prop is false', () => {
-  const renderedComponentType = renderer.create(<DynamicButton isSearched={false} />).toTree();
-  expect(renderedComponentType).toBe('SearchButton');
+describe('DynamicButton', () => {
+  let props;
+  let mountedDynamicButton;
+  const mountDynamicButtonWithProps = () => {
+    mountedDynamicButton = mountedDynamicButton
+      ? mountedDynamicButton
+      : renderer.create(<DynamicButton {...props} />);
+    return mountedDynamicButton;
+  };
+
+  const emptyFunction = () => {};
+
+  const clearComponentAndProps = () => {
+    props = {
+      isSearched: undefined,
+      onClear: undefined,
+      onSearch: undefined,
+    };
+    mountedDynamicButton = undefined;
+  };
+
+
+  beforeEach(() => {
+    clearComponentAndProps();
+  });
+
+  test('renders `SearchButton` when isSearched prop is false', () => {
+    props.isSearched = false;
+    props.onSearch = emptyFunction;
+    mountDynamicButtonWithProps();
+
+    const searchButton = mountedDynamicButton.root.findByType(SearchButton);
+    expect(searchButton).toBeTruthy();
+  });
+
+  test('renders `ClearButton` when isSearched prop is true', () => {
+    props.isSearched = true;
+    props.onClear = emptyFunction;
+    mountDynamicButtonWithProps();
+
+    const clearButton = mountedDynamicButton.root.findByType(ClearButton);
+
+    expect(clearButton).toBeTruthy();
+  });
+
+  // Snapshots
+  test('Snapshot: renders SearchButton when isSearched prop is false', () => {
+    const componentTree = renderer
+      .create(<DynamicButton isSearched={false} onSearch={() => {}} />)
+      .toJSON();
+    expect(componentTree).toMatchSnapshot();
+  });
+
+  test('Snapshot: renders ClearButton when isSearched prop is true', () => {
+    const componentTree = renderer.create(<DynamicButton isSearched onClear={() => {}} />).toJSON();
+    expect(componentTree).toMatchSnapshot();
+  });
 });
