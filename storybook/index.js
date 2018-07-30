c01f23f25de6fc4c2bf244abbafdf77fc4fee31a
SyedFarhan <syedrfarhan2@gmail.com>
2018-07-30 00:44:02 -0400

    Stories in spec, need to add snapshots

--- storybook/index.js
+++ storybook/index.js
@@ -2,16 +2,7 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { getStorybookUI, configure } from '@storybook/react-native';
 // imports to allow spec imports in stories
-import { describe, it, beforeEach } from 'storybook-addon-specifications';
-import expect from 'expect';
-
-window.describe = describe;
-window.beforeEach = beforeEach;
-window.it = it;
-window.test = it;
-window.expect = expect;
-
-
+import './spec-config';
 // import stories
 configure(() => {
   require('./stories');
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react-native';
-import { specs } from 'storybook-addon-specifications'
+import { specs } from 'storybook-addon-specifications';
 
 // import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
