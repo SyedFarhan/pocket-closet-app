ba67ebe41f4bff688907c679e9525f8b951cbeb4
SyedFarhan <syedrfarhan2@gmail.com>
2018-08-01 01:43:52 -0400

    Removed specs from stories as they dont work with JSDOM/enzyme

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -8,17 +8,14 @@ import { specs } from 'storybook-addon-specifications';
 
 import CenterView from './CenterView';
 import DynamicButton from '../../src/features/GarmentSearch/native/DynamicButton';
-import { defaultDynamicButtonSpecs, searchedDynamicButtonSpecs} from '../../src/features/GarmentSearch/native/DynamicButton.test';
 
 storiesOf('DynamicButton', module)
   .add('default', () => {
     const story = <CenterView><DynamicButton isSearched={false} onSearch={() => {}} /></CenterView>;
-    specs(() => defaultDynamicButtonSpecs);
     return story;
   })
   .add('after search', () => {
     const story = <CenterView><DynamicButton isSearched onClear={() => {}} /></CenterView>;
-    specs(() => searchedDynamicButtonSpecs);
     return story;
   });
 
