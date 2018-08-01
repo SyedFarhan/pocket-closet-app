481946670e3c9dd65ffddb5eaecdef3d489e8e13
SyedFarhan <syedrfarhan2@gmail.com>
2018-07-31 22:06:48 -0400

    Created a spec configuration file to allow for tests in stories

---
+++ storybook/spec-config.js
@@ -0,0 +1,8 @@
+import { describe, it, beforeEach } from 'storybook-addon-specifications';
+import expect from 'expect';
+
+window.describe = describe;
+window.beforeEach = beforeEach;
+window.it = it;
+window.test = it;
+window.expect = expect;
