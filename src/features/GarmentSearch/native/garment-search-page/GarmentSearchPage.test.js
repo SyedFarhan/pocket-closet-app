c2af187b8da6dc8db9af82f27c20a4415f1c573c
Syed Farhan <syed.farhan@oliveai.com>
2018-08-30 00:11:43 -0400

    Cleaned up test code

--- src/features/GarmentSearch/native/garment-search-page/GarmentSearchPage.test.js
+++ src/features/GarmentSearch/native/garment-search-page/GarmentSearchPage.test.js
@@ -1,23 +1,32 @@
-/* global describe, it, expect */
+/* global beforeEach, describe, expect, it */
 import React from 'react';
 
 import renderer from 'react-test-renderer';
 
 import GarmentSearchPage from './GarmentSearchPage';
 import StyledHeader from '../../../../native/components/Header';
+import Picker from '../../../../../native-base-theme/components/Picker';
 
 const render = (Component, props) => renderer.create(<Component {...props} />);
 
 describe('GarmentSearchPage', () => {
-  it('should render without errors', () => {
-    const renderedComponent = render(GarmentSearchPage).root;
+  let renderedComponent;
+  let root;
+  let findByType;
 
+  beforeEach(() => {
+    renderedComponent = render(GarmentSearchPage);
+    ({ root } = renderedComponent);
+    findByType = type => root.findByType(type);
+  });
+
+  it('should render without errors', () => {
     expect(renderedComponent).toBeTruthy();
   });
 
   describe('basic structure', () => {
     it('should render a header with title `Garment Search`', () => {
-      const headerComponent = render(GarmentSearchPage).root.findByType(StyledHeader);
+      const headerComponent = findByType(StyledHeader);
       const actualProps = headerComponent.props;
 
       expect(actualProps).toEqual({ description: 'Search for your garment by barcode', title: 'Garment Search'});
